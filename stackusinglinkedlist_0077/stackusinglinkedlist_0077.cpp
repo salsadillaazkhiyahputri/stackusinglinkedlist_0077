37b1a5691682af7686162b69d7de806d78629aeb
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-05-10 14:04:38 +0700

    melihat nilai dari element teratas

--- stackusinglinkedlist_0077/stackusinglinkedlist_0077.cpp
+++ stackusinglinkedlist_0077/stackusinglinkedlist_0077.cpp
@@ -41,4 +41,9 @@ public:
 		cout << "Popped value:" << top->data << endl;
 		top = top->next; // Update the top pointer to the next node
 	}
+
+	// Peek/Top operation: Retrieve the value of the topmost element without removing it
+	void peek() {
+
+	}
 };
